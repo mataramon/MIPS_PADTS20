0dca251e8bc845641c3cc9b1bbef3b0284cfc978
cesarlol <eng.cesar.lopez@gmail.com>
2015-02-21 22:09:14 -0600

    One version of canny working using (IplImage *) as container.
    Images tested: *.jpg & *.bmp
    *.jpg not working
    *.bmp Works but on small sizes
    
    To Run
    ./build_all.sh && ./displaymodificado

---
+++ WEEK2/Canny/displaymodificado.cpp
@@ -0,0 +1,114 @@
+//#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+
+using namespace cv;
+using namespace std;
+
+short* src; //= (short*)(IMAGE_SRC_OFFSET);
+short* dst; //= (short*)(IMAGE_DST_OFFSET);
+
+void convolution(char* kernel, int ampl, int n_k_rows, int n_k_cols, int n_rows, int n_cols)
+{
+	for (int rows = 0; rows < n_rows; rows++) {
+		for (int cols = 0; cols < n_cols; cols++) {
+			int acc = 0;
+			for (int k_rows = 0; k_rows < n_k_rows; k_rows++) {
+				for (int k_cols = 0; k_cols < n_k_cols; k_cols++) {
+					int src_col = ((cols + (k_cols - (n_k_cols / 2))) < 0) ? 0 : cols + (k_cols - (n_k_cols / 2));
+					int src_row = ((rows + (k_rows - (n_k_rows / 2))) < 0) ? 0 : rows + (k_rows - (n_k_rows / 2));
+					acc += kernel[k_cols + (k_rows * n_k_cols)] * src[src_col + (src_row * n_cols)];
+				}
+			}
+			dst[cols + (rows * n_cols)] = dst[cols + (rows * n_cols)] + (acc / ampl);
+		}
+	}
+}
+
+void sobel_gx(int width, int height)
+{
+	char kernel[3 * 3] =
+	{ -1, 0, +1,
+	-2, 0, +2,
+	-1, 0, +1 };
+
+	convolution(&kernel[0], 1, 3, 3, height, width);
+}
+
+void sobel_gy(int width, int height)
+{
+	char kernel[3 * 3] =
+	{ -1, -2, -1,
+	   0, 0, 0,
+	  +1, +2, +1 };
+
+	convolution(&kernel[0], 1, 3, 3, height, width);
+}
+
+void canny_simp(int width, int height)
+{
+	//int width = s.width;
+	//int height = s.height;
+
+	// Run Filter
+	sobel_gy(width, height);
+
+	// Run Filter
+	sobel_gx(width, height);
+
+}
+
+int main( int argc, char** argv )
+{
+  
+	const char * f_SRC = "android.bmp";
+	const char * f_DST = "output_5.bmp";
+
+	IplImage *img = cvLoadImage(f_SRC, CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage *imgDst = cvLoadImage(f_SRC, CV_LOAD_IMAGE_GRAYSCALE);
+	//IplImage *test = cvLoadImage(f_SRC, CV_LOAD_IMAGE_GRAYSCALE);        
+	
+	//Size s = img.size();
+    //Mat imageDst(s,CV_8UC1);
+    //imageDst = imread("./scarlet.bmp", CV_LOAD_IMAGE_GRAYSCALE); 
+
+	cout << "width " << img->width << endl;
+	cout << "height " << img->height << endl << endl;
+
+    src = (short*)(img->imageData);
+    dst = (short*)(imgDst->imageData);
+    
+	char *sweepdata = imgDst->imageData;
+	int count=0;
+	cout<< "Before Canny" << endl;
+	cout << "&img->Data: " << &img->imageData << endl;
+	cout << "&imgDst->Data: " << &imgDst->imageData << endl;
+	cout << "&src->Data: " << &src << endl;
+	cout << "&dst->Data: " << &dst << endl << endl;
+
+    if(! img->imageData )                          
+    {
+        cout <<  "Could not open or find the image" << endl ;
+        return -1;
+    }
+	canny_simp(img->width, img->height);
+	/*while(*sweepdata != NULL){
+		*sweepdata++;		
+		count++;		
+	//	cout << *sweepdata++;   // Sweeps all the content data of the image!
+	}*/
+
+   	// canny_simp(img->width, img->height);
+	//	canny_simp(img->width, img->height);
+
+	if(!cvSaveImage(f_DST,imgDst)) cout<<"Could not save: "<< f_DST << endl;
+
+	cout<< "Canny correctly applied" << endl;
+	cout << "&img->Data: " << &img->imageData << endl;
+	cout << "&imgDst->Data: " << &imgDst->imageData << endl;
+	cout << "&src->Data: " << &src << endl;
+	cout << "&dst->Data: " << &dst << endl;
+ 	//cout<< "SizeofData: " << count << endl;
+    return 0;
+}
