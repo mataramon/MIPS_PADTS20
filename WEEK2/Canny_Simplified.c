5babbd0fba408a3bc597b8148e09bfecc51b8fd1
cesarlol <eng.cesar.lopez@gmail.com>
2015-02-24 16:42:18 -0600

    Modificaciones a Canny, funcionando.
    Assembler = Canny_Simplified.s

--- WEEK2/Canny_Simplified.c
+++ WEEK2/Canny_Simplified.c
@@ -1,25 +1,15 @@
-//#define IMAGE_SRC_OFFSET 120
-//#define IMAGE_DST_OFFSET 70
-short* src = (short*)(IMAGE_SRC_OFFSET);
-short* dst = (short*)(IMAGE_DST_OFFSET);
+char* src = (char*)( 0x00002000); //0x0
+char* dst = (char*)( 0x00004000);
 
 void convolution(char* kernel, int ampl, int n_k_rows, int n_k_cols, int n_rows, int n_cols)
 {
-    int rows;
-    int cols;
-    int acc;
-    int k_cols;
-    int k_rows ;
-    int src_col;
-    int src_row;
-    
-	for (rows = 0; rows < n_rows; rows++) {
-		for (cols = 0; cols < n_cols; cols++) {
-			acc = 0;
-			for (k_rows = 0; k_rows < n_k_rows; k_rows++) {
-				for (k_cols = 0; k_cols < n_k_cols; k_cols++) {
-					src_col = ((cols + (k_cols - (n_k_cols / 2))) < 0) ? 0 : cols + (k_cols - (n_k_cols / 2));
-					src_row = ((rows + (k_rows - (n_k_rows / 2))) < 0) ? 0 : rows + (k_rows - (n_k_rows / 2));
+	for (int rows = 0; rows < n_rows; rows++) {
+		for (int cols = 0; cols < n_cols; cols++) {
+			int acc = 0;
+			for (int k_rows = 0; k_rows < n_k_rows; k_rows++) {
+				for (int k_cols = 0; k_cols < n_k_cols; k_cols++) {
+					int src_col = ((cols + (k_cols - (n_k_cols / 2))) < 0) ? 0 : cols + (k_cols - (n_k_cols / 2));
+					int src_row = ((rows + (k_rows - (n_k_rows / 2))) < 0) ? 0 : rows + (k_rows - (n_k_rows / 2));
 					acc += kernel[k_cols + (k_rows * n_k_cols)] * src[src_col + (src_row * n_cols)];
 				}
 			}
@@ -50,8 +40,8 @@ void sobel_gy(int width, int height)
 
 int main ()
 {
-	int width = 160;
-	int height = 120;
+	int width = 96;
+	int height = 96;
 	int i;
 
 	// Run Filter
